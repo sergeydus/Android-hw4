2cf18d44921a52009482af86f5837d28ee02f778
alexdushevski <39350701+Tehheavy@users.noreply.github.com>
2019-06-16 15:10:36 +0300

    Update DonationActivity.java

--- app/src/main/java/com/example/android_hw4/DonationActivity.java
+++ app/src/main/java/com/example/android_hw4/DonationActivity.java
@@ -1,6 +1,7 @@
 package com.example.android_hw4;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,31 +15,39 @@ import java.nio.DoubleBuffer;
 import pl.droidsonroids.gif.GifImageView;
 
 public class DonationActivity extends AppCompatActivity {
-    double sum_donation;
+    float sum_donation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donation);
-        sum_donation=getIntent().getDoubleExtra("donation",0);
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        sum_donation=(float)getIntent().getDoubleExtra("donation",0);
         TextView tv=findViewById(R.id.total_donate);
-        tv.setText(Double.toString(sum_donation)+"₪");
+        float total=(float)sharedPreferences.getFloat("value",(float)0);
+        tv.setText(String.valueOf((float)total)+"₪");
     }
     public void donate(View view){
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
         EditText ed1= findViewById(R.id.donate);
         TextView tv1=findViewById(R.id.total_donate);
         TextView tv3=findViewById(R.id.enteryourdonation);
         Button b1= findViewById(R.id.clickDonate);
         GifImageView imageView= findViewById(R.id.dancing);
         TextView tv2= findViewById(R.id.thanks);
-        Double num1= Double.valueOf(ed1.getText().toString());
+        float num1= Float.valueOf(ed1.getText().toString());
 
         ed1.setVisibility(View.GONE);
         b1.setVisibility(View.GONE);
         tv3.setVisibility(View.GONE);
 
-        Double sum=num1+sum_donation;
+        float sum=num1+sum_donation;
+        editor.putFloat("value",(float)sum_donation+(float)num1);
+        editor.commit();
         sum_donation=sum;
-        tv1.setText(sum.toString()+"₪");
+        tv1.setText(String.valueOf(sum)+"₪");
         imageView.setVisibility(View.VISIBLE);
         tv2.setVisibility(View.VISIBLE);
 
@@ -47,8 +56,5 @@ public class DonationActivity extends AppCompatActivity {
         Intent intent= new Intent(this, NoteActivity.class);
         intent.putExtra("donation",sum_donation);
         startActivity(intent);
-    }
-    public void onBackPressed(){
-
     }
 }
